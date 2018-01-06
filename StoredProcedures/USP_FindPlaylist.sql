9e61ef7c86f18abc3f46a9d3c01337c26a665fc0
Georg Schabasser <if16b064@technikum-wien.at>
2018-01-06 16:41:23 +0100

    USP_FindPlaylist implemented

--- StoredProcedures/USP_FindPlaylist.sql
+++ StoredProcedures/USP_FindPlaylist.sql
@@ -6,50 +6,53 @@ GO
 -- using the object id to check if the stored procedure with the given name already --
 -- exists. If it exists, it will be dropped from the context.                       --
 --------------------------------------------------------------------------------------
-IF OBJECT_ID('USP_', 'P') IS NOT NULL
-	DROP PROCEDURE [USP_];
+IF OBJECT_ID('USP_FindPlaylist', 'P') IS NOT NULL
+	DROP PROCEDURE [USP_FindPlaylist];
 GO
 
 
 ----------------------------------------------------------------
 -- creation of a stored procedure with the following purpose: --
---    <PURPOSE>                                               --
+--	Get Titles of Playlists that match the search parameter   --
+--	if parameter is empty, return all playlists               --
 ----------------------------------------------------------------
-CREATE PROCEDURE [USP_] (
-	@Param1		NVARCHAR(32),
-	@Param2		FLOAT,
-	@Result		INT = 0 OUTPUT
+CREATE PROCEDURE [USP_FindPlaylist] (
+	@Playlistname	NVARCHAR(32)
 )
 AS BEGIN
 	-- no console outputs are needed here
 	SET NOCOUNT ON;
-
-	-- use transactions to rollback invalid statements
-	BEGIN TRANSACTION;
-		-- stored procedure code here
-	ROLLBACK;
+	BEGIN TRY;
+		IF (@Playlistname = '')
+			SELECT [playlist_id], [description], [title], [name] as 'genre', [username] as 'creator'
+			FROM [playlist] AS p, [genre] AS g, [user] as u
+			WHERE p.[fk_genre_id] = g.[genre_id] AND p.[fk_user_id] = u.[user_id];
+		ELSE
+			SELECT [playlist_id], [description], [title], [name] as 'genre', [username] as 'creator'
+			FROM [playlist] AS p, [genre] AS g, [user] as u
+			WHERE [title] LIKE Concat('%', @Playlistname, '%') AND p.[fk_genre_id] = g.[genre_id] AND p.[fk_user_id] = u.[user_id];
+	END TRY BEGIN CATCH;
+		RETURN 1;
+	END CATCH;
 END
 GO
 
 ---------------------------------------------------------------------------
 -- run the stored procedure to check correct behaviour and return values --
 ---------------------------------------------------------------------------
-DECLARE @Out INT;
-EXEC dbo.[USP_]
-	@Param1 = 'Hello World',
-	@Param2 = 10,
-	@Result = @Out OUTPUT;
-SELECT @Out;
 
+-- with specified playlist title
+EXEC dbo.[USP_FindPlaylist]
+	@Playlistname = 'out';
+
+-- get all playlists when title is empty
+EXEC dbo.[USP_FindPlaylist]
+	@Playlistname = '';
 
 ---------------------------------------------------------------------------
 -- add a test dataset to be sure the results given by the stored         --
 -- procedure are correct.                                                --
 ---------------------------------------------------------------------------
-INSERT INTO
-	[table] ([col1], [col2], [coln])
-	VALUES (val1, val2, valn);
-
--- Delete Test Dataset
-DELETE FROM [table]
-WHERE [col1] = val1;
+INSERT INTO [playlist] ([description], [title], [fk_genre_id], [fk_user_id]) VALUES ('Very nice grooves', 'Groovy Playlist', 2, 1);
+INSERT INTO [playlist] ([description], [title], [fk_genre_id], [fk_user_id]) VALUES ('Trash', 'Bad Playlist', 3, 1);
+INSERT INTO [playlist] ([description], [title], [fk_genre_id], [fk_user_id]) VALUES ('Trying something new, hopy you like it', 'Diving in the Stars', 4, 1);
